6f2d3a4e623fd74e255502fef1aab0f116096544
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-08-31 12:20:30 +0200

    Add two sniffs to find and remove unused "use" clauses

---
+++ Wikibase/Tests/Namespaces/UnusedUse.php
@@ -0,0 +1,63 @@
+<?php
+
+use Sub\Unused;
+use UsedButBadCapitalization;
+use Wikibase\AnotherBadCapitalization;
+use UnusedMain ;
+use UsedMain ;
+use AnotherUnusedMain;
+use Sub\Used;
+use Wikibase\Sub\UsedInAnArray;
+use UsedAndCommented;
+use MentionedInComment;
+use AppearsToBeUnused as ButIsUsed;
+use Sub\AnotherAlias as /* Comments should be skipped */WithAComment;
+use Sub\AndAnotherAlias as /** @note PHPDocs should also be skipped */WithADocComment;
+use UsedAfterParamName;
+use UsedAfterVarName;
+use UsedAfterPipe;
+use UsedTrait;
+use UsedExpectedException;
+use UsedThrows;
+use UsedReturn;
+
+/**
+ * @group Unused
+ */
+class Example implements UsedMain {
+
+	use UsedTrait;
+
+	/**
+	 * @type Used
+	 */
+	private $prop;
+
+	/**
+	 * @var UsedAndCommented with a comment
+	 */
+	private $prop;
+
+	/**
+	 * @var UsedButBadCapiTaliZation
+	 */
+	private $prop;
+
+	/**
+	 * @expectedException UsedExpectedException
+	 * @param int[]|UsedInAnArray[] $arg
+	 * @param bool $arg MentionedInComment should not count as usage
+	 * @param $arg UsedAfterParamName[]|null
+	 * @throws UsedThrows
+	 * @return UsedReturn
+	 */
+	private function process( array $arg, AnotherBadCapiTaliZation $arg ) {
+		/* @var ButIsUsed $var */
+		/* @var $var UsedAfterVarName[]|null */
+		/* @var $var null|UsedAfterPipe */
+		/* @var $var bool MentionedInComment should not count as usage */
+		$withAComment = new WithAComment();
+		$withADocComment = new WithADocComment();
+	}
+
+}
